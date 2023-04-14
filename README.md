66d71bd21271848efe37753dcc1de9d95ab80ded
xii <xii@xii.xii>
2023-04-14 22:24:32 +0200

    sec

---
+++ README.md
@@ -0,0 +1,19 @@
+
+
+
+
+
+# Introduction Ai npc movement
+
+-  I itereated from using the hit variable of type RaycastHit in our first script clickmove.cs to create a player click function #which instructs our player Agent on where to move.Moved to creating a steer path powered agent which is able to follow a given path, this is all possible because of one of our linear interpolation technique (Slerp [Spherical Linear intERPolation])
+
+#Credit 
+
+Beginning Game AI with Unity
+
+# Screenshots
+
+![screenshot](ai.png)
+
+
+
---
+++ clickmove.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clickmove : MonoBehaviour
+{
+   Vector3 goal;
+
+   float speed = 1.0f;
+   float accuracy = 0.5f;
+   float rotSpeed = 2.0f;
+
+    void Start() {
+     goal = this.transform.position;
+    
+   }
+
+    // Update is called once per frame
+    void Update()
+    {
+    RaycastHit hit;
+    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    
+   if (Physics.Raycast(ray, out hit) && Input.GetMouseButtonDown(0))
+           
+ {
+      
+   goal = new Vector3(hit.point.x, this.
+   transform.position.y, hit.point.z);           }
+
+         Vector3 direction = goal - this.transform.position;
+this.transform.rotation = Quaternion.Slerp(this.transform.
+rotation, Quaternion.LookRotation(direction), Time.
+deltaTime*rotSpeed);
+         
+    if(Vector3.Distance(transform.position, goal) >accuracy)
+           {
+             
+      this.transform.Translate(0,0, speed*Time.deltaTime);
+          }
+      }
+}
